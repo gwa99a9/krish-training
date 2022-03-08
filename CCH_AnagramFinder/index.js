cfaae317e6f2208cd68cac2b9e2cd54532f428c5
Jeewaka Manohara <jamesheller.jeewake@gmail.com>
2022-03-08 18:50:11 +0530

    changed from stdin to readline

--- CCH_AnagramFinder/index.js
+++ CCH_AnagramFinder/index.js
@@ -1,11 +1,24 @@
-console.log('Enter two check those words are Anagram.')
-let rawString = 'act cap'
-const rawSplit = rawString.split(' ')
 
-const word1 = Array.from(rawSplit[0]).sort();
-const word2 = Array.from(rawSplit[1]).sort();
+const readline = require('readline');
 
-console.log(JSON.stringify(word1) === JSON.stringify(word2));
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-let rawInt = [12, 43, 565, 21, 555, 677].sort();
-console.log(rawInt[rawInt.length - 3]);
+rl.question('Enter two words to check if they are Anagram.Please use : as delimiter - ', (data) => {
+    const rawString = data.toLowerCase().replace(/ /g, '');
+    const rawSplit = rawString.split(':')
+    const word1 = Array.from(rawSplit[0]).sort().join('')
+    const word2 = Array.from(rawSplit[1]).sort().join('')
+    if (word1 === word2) {
+        console.log(`${word1} and ${word1} are anagram words.`);
+    } else {
+        console.log('These two words are not anagram.');
+    }
+    rl.close();
+});
+
+
+// let rawInt = [12, 43, 565, 21, 555, 677].sort()
+// console.log(rawInt[rawInt.length - 3])
