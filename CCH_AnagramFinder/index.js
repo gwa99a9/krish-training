d3850f3c1ac13756adf8b8fc94b5fe678eecbb4a
Jeewaka Manohara <jamesheller.jeewake@gmail.com>
2022-03-08 21:25:54 +0530

    minor code fixes

--- CCH_AnagramFinder/index.js
+++ CCH_AnagramFinder/index.js
@@ -12,13 +12,9 @@ rl.question('Enter two words to check if they are Anagram.Please use : as delimi
     const word1 = Array.from(rawSplit[0]).sort().join('')
     const word2 = Array.from(rawSplit[1]).sort().join('')
     if (word1 === word2) {
-        console.log(`${word1} and ${word1} are anagram words.`);
+        console.log(`${word1} and ${word2} are anagram words.`);
     } else {
         console.log('These two words are not anagram.');
     }
     rl.close();
 });
-
-
-// let rawInt = [12, 43, 565, 21, 555, 677].sort()
-// console.log(rawInt[rawInt.length - 3])
